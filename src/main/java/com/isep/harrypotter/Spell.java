6b10a341bdb3f742b4f02d0bbe994810302c3aef
charlottedvl <charlotte.deville@eleve.isep.fr>
2023-03-12 17:17:01 +0100

    Modification for Sorcery class : you can misspell a spell and it still works

--- src/main/java/com/isep/harrypotter/Spell.java
+++ src/main/java/com/isep/harrypotter/Spell.java
@@ -33,7 +33,7 @@ public class Spell extends AbstractSpell{
 
 
 
-    public static String attendSpellClass(Year year, ArrayList<Spell> spells, Wizard player){
+    public static Spell attendSpellClass(Year year, ArrayList<Spell> spells, Wizard player){
         System.out.println("You have chosen to attend the sorcery class");
         System.out.println("This year, you can learn one of the many spells below. Please enter the name of the spell you want to learn");
         int i = 1;
@@ -45,19 +45,21 @@ public class Spell extends AbstractSpell{
         }
         Scanner scanner = new Scanner(System.in);
         String nameSpell = scanner.nextLine();
-        return nameSpell;
+        return searchSpell(nameSpell, spells, year, player);
     }
 
     public static Spell searchSpell(String nameSpell, ArrayList<Spell> spells, Year year, Wizard player){
-        for (Spell spell : spells) {
-            if (spell.getName().equals(nameSpell)) {
-                return spell;
+        while (true) {
+            for (Spell spell : spells) {
+                if (spell.getName().equalsIgnoreCase(nameSpell)) {
+                    return spell;
+                }
             }
+            System.out.println("Please enter a valid spell");
+            Spell spell = attendSpellClass(year, spells, player);
         }
-        System.out.println("An error occurred, please try again");
-        attendSpellClass(year, spells, player);
-        return null;
     }
+
     public static void learnSpell(Spell spell, Wizard player, Year year, ArrayList<Spell> spells){
         System.out.println("Are you sur you want to learn " + spell.getName() + " ?");
         System.out.println("1. Yes");
